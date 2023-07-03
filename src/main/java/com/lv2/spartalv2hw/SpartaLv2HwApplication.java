d1ede1e7baacaee1825cd5a3ba6afed739c24771
PARKJUNYOUNG\USER <yyijs0309@gmail.com>
2023-07-03 18:07:18 +0900

    init commit

---
+++ src/main/java/com/lv2/spartalv2hw/SpartaLv2HwApplication.java
@@ -0,0 +1,13 @@
+package com.lv2.spartalv2hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaLv2HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaLv2HwApplication.class, args);
+	}
+
+}
